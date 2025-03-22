a90e771dcb641bdd4773273c834dc8921a712b3e
MAITUMELO RADIPODI <mradipodi@gmail.com>
2025-03-22 17:11:02 +0200

    feat: register form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,42 @@
 import Navbar from "./components/Navbar"
-import {Routes,Route} from "react-router-dom"
+import {Routes,Route, Navigate} from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import ProfilePage from "./pages/ProfilePage.jsx"
 import SettinsPage from "./pages/SettinsPage.jsx"
 import SignInPage from "./pages/SignInPage.jsx"
 import SignUpPage from "./pages/SignUpPage.jsx"
-
+import { useAuthStore } from "./store/useAuthStore.js"
+import { useEffect } from "react"
+import {Loader} from "lucide-react"
 
 const App = () =>{
+
+  const {authUser,checkAuth,isCheckingAuth}= useAuthStore()
+  useEffect(() =>{
+    checkAuth()
+  },[checkAuth])
+
+  console.log(authUser,"I AM AUTH USER")
+
+  if(isCheckingAuth && !authUser){
+    return(
+      <div className="flex justify-center items-center h-screen">
+        <Loader className="size-10 animate-spin"/>
+      </div>
+    )
+
+  }
+
   return (
   <div>
   <Navbar/>
 
   <Routes>
-  <Route path="/" element={<HomePage/>}/>
-  <Route path="/signup" element={<SignUpPage/>}/>
-  <Route path="/login" element={<SignInPage/>}/>
+  <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login"/>}/>
+  <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/"/>}/>
+  <Route path="/login" element={!authUser ? <SignInPage/>: <Navigate to="/"/>}/>
   <Route path="/settings" element={<SettinsPage/>}/>
-  <Route path="/profile" element={<ProfilePage/>}/>
+  <Route path="/profile" element={authUser  ? <ProfilePage/>: <Navigate to="/login"/>}/>
 
   </Routes>
   </div>
