6f9eebc324b4cc21c69e998a5bc6bf1d96d8b365
MAITUMELO RADIPODI <mradipodi@gmail.com>
2025-03-25 23:21:27 +0200

    Project Completed

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,15 @@ import connectDB from './config/dbConnect.js'
 import authRoutes from './routes/authRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
 import {app,server} from './config/socket.js'
-
+import path from 'path'
 //config
 dotenv.config();
 
 //initalize app
 
 const PORT  = process.env.PORT
+
+const  __dirname = path.resolve()
 
 
 //middleware
@@ -26,6 +28,13 @@ app.use(cors({
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"))
+    })
+}
 
 server.listen(PORT,() =>{ 
     console.log('Server is running on port '+ PORT)
